61a16cc60b83fd4007aa3fb7211f605044783656
Kin Seng Chau <kinseng529@gmail.com>
2017-09-26 02:52:27 -0700

    hw2 finish

--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -8,5 +8,11 @@ class Stringify
 
   def what_am_i
   	# YOUR IMPLEMENTATION HERE
+  	if @name.blank? || @adjective.blank?
+  		@text = "You are nothing!"
+  	else
+  		@text = @name + " is so " + @adjective
+  	end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'age', to: 'pages#age'
+
+  # Q1a: routes to the stringify method in pages_controller.rb
+  post 'stringify', to: 'pages#stringify'
+
+  # Q3: handle the form submission
+  put 'age', to: 'pages#person'
+
+  get 'me', to: 'pages#me'
+
 end
